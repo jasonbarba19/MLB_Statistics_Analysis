90569ddfeb9f98398b6149ec50336080abd6e4bf
jasonbarba <jrbarba@calpoly.edu>
2023-05-08 16:44:11 -0700

    Batters Loaded in

--- Batter.py
+++ Batter.py
@@ -1,1 +1,26 @@
 # Class Representing Each Batter
+
+class Batter:
+	def __init__(self, first_name, last_name, games, atbats,
+	       runs, hits, doubles, triples, homeruns, rbi, tb, bb, so, sb, ave, obp, slg, ops, war):
+		self.first_name = first_name
+		self.last_name = last_name
+		self.games = games
+		self.atbats = atbats
+		self.runs = runs
+		self.hits = hits
+		self.doubles = doubles
+		self.triples = triples
+		self.homeruns = homeruns
+		self.rbi = rbi
+		self.tb = tb
+		self.bb = bb
+		self.so = so
+		self.sb = sb
+		self.ave = ave
+		self.obp = obp
+		self.slg = slg
+		self.ops = ops
+
+	def __str__(self):
+		return self.first_name + " " + self.last_name + " " + self.grade + " " + self.classroom + " " + self.bus + " " + self.gpa + " " + self.teacher_last + " " + self.teacher_first
--- batter_analyze.py
+++ batter_analyze.py
@@ -1,1 +1,18 @@
 # Major League Baseball Statistical Analysis for Batters
+import csv
+def main():
+	batters = []
+	try:
+		with open("/Users/jasonbarba/Projects/MLB_Statistics_Analysis/WhiteSox_Batting_2022.csv", 'r') as file:
+			csvreader = csv.reader(file)
+			for batter in csvreader:
+				batters.append(batter)
+	except:
+		print("Could not open file")
+		return None
+	# print(contents)
+	print(batters[1])
+	
+
+if __name__ == "__main__":
+	main()
